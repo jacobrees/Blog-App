1c6c78e43c79fb80802c956c2d8830fcc3bfc8b8
jacobrees <jacob.rees97@yahoo.com>
2022-11-09 10:43:13 +0000

    Fix all ruby associated linting errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,47 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all.order("created_at DESC")
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def new
-        @post = Post.new
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
+  end
 
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render 'new'
-        end
-    end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+  private
 
-    def edit
-        @post = Post.find(params[:id])
-    end
-
-    def update
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-            redirect_to @post
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path
-    end
-
-    private
-
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
-
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
